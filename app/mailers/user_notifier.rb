c6a9a3bbeac897b91ff11733df2770281aaa760d
Andrei Chernykh <andrei.chernykh@gmail.com>
2013-06-05 11:46:38 +0400

    mailer production url

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -38,7 +38,7 @@ Reactor2::App.mailer :user_notifier do
   email :confirmation do |user|
     from 'support@improva.com'
     to user.email
-    host = PADRINO_ENV == 'production' ? 'ec2-54-245-59-116.us-west-2.compute.amazonaws.com' : 'localhost:3000'
+    host = PADRINO_ENV == 'production' ? 'ec2-54-244-82-4.us-west-2.compute.amazonaws.com' : 'localhost:3000'
     link = "http://#{host}/api/v1/confirmation/#{user.hashs}"
     subject 'Improva: sign up confirmation'
     locals user: user, link: link
